8040595d371a4e83dae2ef8ddf6e33c392071ddc
ynunokawa <yusuke_nunokawa@esrij.com>
2016-08-16 17:32:40 +0900

    layer name rules

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,9 @@
 var appConfig = {
   webmapId: 'fb531718fd8d46dca745625d16954f1b', // ArcGIS Web マップ ID: http://www.arcgis.com/home/webmap/viewer.html?webmap=fb531718fd8d46dca745625d16954f1b
+  // **レイヤー命名規則**
+  // 23区別保育園レイヤー: '○○区'
+  // 定員レイヤー: '保育園（定員）'
+  // 東京23区境界: '東京都（23区）'
+  // 背景地図: [何でもOK]
   defaultAreaName: '練馬区'
 };
