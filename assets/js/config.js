23a3546eef11068401e31ca0c311262a920543d7
ynunokawa <yusuke_nunokawa@esrij.com>
2016-08-18 11:30:18 +0900

    add ui to select area

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,6 @@
 var appConfig = {
-  webmapId: 'fb531718fd8d46dca745625d16954f1b', // ArcGIS Web マップ ID: http://www.arcgis.com/home/webmap/viewer.html?webmap=fb531718fd8d46dca745625d16954f1b
+  areaMapId: '193d0fd453654099a85853ec8f8413f4',
+  hoikuenMapId: 'fb531718fd8d46dca745625d16954f1b', // ArcGIS Web マップ ID: http://www.arcgis.com/home/webmap/viewer.html?webmap=fb531718fd8d46dca745625d16954f1b
   // **レイヤー命名規則**
   // 23区別保育園レイヤー: '○○区'
   // 定員レイヤー: '保育園（定員）'
---
+++ assets/js/selectArea.js
@@ -0,0 +1,37 @@
+var targetAreaName = '';
+
+$(document).ready(function(){
+    console.log(appConfig);
+    var areaMap = L.esri.webMap(appConfig.areaMapId, { map: L.map('area-map', { zoomControl: false }) });
+    var modal = $('#modal-select-area');
+    areaMap.on('load', areaMapLoaded);
+
+    function areaMapLoaded() {
+        console.log(areaMap.layers[1].layer);
+        areaMap._map.removeLayer(areaMap.layers[0].layer);
+
+        for(key in areaMap.layers[1].layer._layers){
+            if(areaMap.layers[1].layer._layers[key]._cache !== undefined) {
+                var tokyo = areaMap.layers[1].layer._layers[key];
+                console.log(tokyo);
+                tokyo.on('click', function(e) {
+                    console.log(e);
+                    console.log(e.originalEvent.target);
+                    var target = $(e.originalEvent.target);
+                    var areas = $('div.modal-body > div > div > div > svg > g > path');
+                    console.log(target);
+                    console.log(areas);
+                    areas.attr('class', 'leaflet-interactive');
+                    target.attr('class', 'leaflet-interactive selected-area');
+                    targetAreaName = e.layer.feature.properties['CSS_NAME'];
+                    $('#target-area-name').text(targetAreaName);
+                });
+            }
+        }
+    }
+
+    modal.on('shown.bs.modal', function () {
+        console.log('modal shown');
+    });
+    modal.modal({ backdrop: false, show: true });
+});
